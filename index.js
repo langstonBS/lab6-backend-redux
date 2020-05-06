1e47e1559f8aefd8e68269200af62cda157c6a03
langstonBS <langston.beckwithstanley@gmail.com>
2020-05-06 11:30:26 -0700

    fixed app and eslit

--- index.js
+++ index.js
@@ -5,16 +5,19 @@ const express = require('express');
 
 const cors = require('cors');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3003;
 
 const app = express();
 
 app.use(cors());
 
-app.git('/', (req, res) => {
+app.get('/', (req, res) => {
     res.json({ hellow: 'world' });
 });
 
-app.listen(PORT, () => console.log(PORT));
+app.listen(PORT, () => console.log(`running on port ${PORT}`));
+
+
+// https://intense-spire-73746.herokuapp.com
 
 
